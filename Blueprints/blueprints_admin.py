10664d2bc70a98dd063b6c04b795a0e59c92419e
Tanmay Potbhare <102863875+TanmayPotbhare@users.noreply.github.com>
2024-11-15 18:01:45 +0530

    Latest Commit 15th November at 18:01

--- Blueprints/blueprints_admin.py
+++ Blueprints/blueprints_admin.py
@@ -1,4 +1,7 @@
 from PythonFiles.AdminPages.adminlogin import adminlogin_blueprint, adminlogin_auth
+from PythonFiles.AdminPages.AdminLevels.admin_level_one import adminlevelone_blueprint, adminlevelone_auth
+from PythonFiles.AdminPages.AdminLevels.admin_level_two import adminleveltwo_blueprint, adminleveltwo_auth
+from PythonFiles.AdminPages.AdminLevels.admin_level_three import adminlevelthree_blueprint, adminlevelthree_auth
 
 
 # Function to register admin blueprints
@@ -6,3 +9,15 @@ def admin_blueprints(app, mail):
     # Admin Login
     adminlogin_auth(app)
     app.register_blueprint(adminlogin_blueprint)
+
+    # Admin Level One
+    adminlevelone_auth(app, mail)
+    app.register_blueprint(adminlevelone_blueprint)
+
+    # Admin Level One
+    adminleveltwo_auth(app, mail)
+    app.register_blueprint(adminleveltwo_blueprint)
+
+    # Admin Level Three
+    adminlevelthree_auth(app, mail)
+    app.register_blueprint(adminlevelthree_blueprint)
