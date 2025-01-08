0e04c0d33661bfa51d5ebfed0bdedf838ed028d8
Tanmay Potbhare <102863875+TanmayPotbhare@users.noreply.github.com>
2025-01-08 17:36:13 +0530

    Latest Commit 8th January at 17:36

--- PythonFiles/CandidatePages/candidate_dashboard.py
+++ PythonFiles/CandidatePages/candidate_dashboard.py
@@ -52,37 +52,6 @@ def candidate_dashboard_auth(app):
         return render_template('CandidatePages/candidate_dashboard.html', title="My Profile", records=records,
                                user=user, photo=photo, finally_approved=finally_approved)
 
-    @candidate_dashboard_blueprint.route('/installment_details')
-    def installment_details():
-        if not session.get('logged_in_from_login'):
-            # Redirect to the admin login page if the user is not logged in
-            return redirect(url_for('adminlogin.admin_login'))
-
-        email = session['email']
-
-        host = HostConfig.host
-        connect_param = ConnectParam(host)
-        cnx, cursor = connect_param.connect(use_dict=True)
-
-        sql = """SELECT * FROM application_page WHERE email = %s"""
-        cursor.execute(sql, (email,))
-        records = cursor.fetchall()
-
-        # Check if the user is approved for fellowship no matter the year to show the desired sidebar.
-        if records[0]['final_approval'] == 'accepted':
-            finally_approved = 'approved'
-        else:
-            finally_approved = 'pending'
-
-        if records:
-            user = records[0]['first_name'] + ' ' + records[0]['last_name']
-            photo = records[0]['applicant_photo']
-        else:
-            user = "Admin"
-            photo = '/static/assets/img/default_user.png'
-        return render_template('CandidatePages/installment_details.html', title="Installment Details", records=records,
-                               user=user, photo=photo, finally_approved=finally_approved)
-
     @candidate_dashboard_blueprint.route('/adhaar_seeding')
     def adhaar_seeding():
 
